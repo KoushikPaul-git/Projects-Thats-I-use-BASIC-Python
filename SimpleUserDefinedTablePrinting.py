1d0a67aed147903387cf0995196356e14ca02326
Koushik Paul <koushikpaul@gmail.com>
2025-11-05 14:16:32 +0530

    Simple User Defined Table Printing Basic learning Project .

---
+++ SimpleUserDefinedTablePrinting.py
@@ -0,0 +1,11 @@
+# The Table printing using While Loop 
+
+
+number = int(input("Enter the number: "))
+
+i = 1
+while (i<11):
+    print(f"{number} X {i} = {number*i}")
+    i+=1
+    
+# Thank you for read this code. 
